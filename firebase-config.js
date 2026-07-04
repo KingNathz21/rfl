6f759bd447f63a90c95418925b2ee0fba719b611
KingNathz21 <kingnathan210509@outlook.com>
2026-07-04 08:41:59 +0100

    Add Firebase project configuration

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.RouteFlowFirebaseConfig = {
+  apiKey: "AIzaSyAMZ5bEx4no7psjpzfMOQFyb2ukZ9ymavQ",
+  authDomain: "rfldon.firebaseapp.com",
+  projectId: "rfldon",
+  storageBucket: "rfldon.firebasestorage.app",
+  messagingSenderId: "1097447189862",
+  appId: "1:1097447189862:web:a83dc670673e91100eaca7",
+  measurementId: "G-CNCXXN7BXV"
+};
